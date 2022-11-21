9cebfae7047d5bab7b6ef0122d5feb77e008be22
guifami <guiruiz05112003@gmail.com>
2022-11-21 19:10:10 -0300

    [ADD] - Starting Game pages

--- Frontend/Application/src/app/app.module.ts
+++ Frontend/Application/src/app/app.module.ts
@@ -27,7 +27,6 @@ import { LineChartComponent } from './components/charts/line-chart/line-chart.co
 import { GridGraficosComponent } from './components/grid-graficos/grid-graficos.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SubscriptionComponent } from './pages/subscription/subscription.component';
-import { PlayerCardComponent } from './components/player-card/player-card.component';
 // Games Components
 import { LeagueOfLegendsComponent } from './pages/Jogos/league-of-legends/league-of-legends.component';
 import { TeamfightTacticsComponent } from './pages/Jogos/teamfight-tactics/teamfight-tactics.component';
@@ -72,8 +71,7 @@ import { JogosComponent } from './pages/Jogos/Jogos.component';
     DoughnutChartComponent,
     LineChartComponent,
     AboutComponent,
-    SubscriptionComponent,
-    PlayerCardComponent
+    SubscriptionComponent
   ],
   imports: [
     BrowserModule,
